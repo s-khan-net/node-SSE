2c0f3a60621fb49a063efed3c1b4f5a2c3e7c90f
Khan <798425@cognizant.com>
2020-06-06 22:18:37 +0530

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var parser = require('body-parser');
 var eventemitter = require('events');
+
 var cors = require('cors');
 const Stream  = new eventemitter();
 
@@ -13,10 +14,7 @@ app.use(
         extended:true
     })
 )
-app.get('/',function(res,res,next){
-    res.send('express');
-})
-app.get('/routes',function(req,res){
+app.get('/',function(req,res,next){
     res.writeHead(200,{
         'Content-Type':'text/event-stream',
         'Cache-Control':'no-cache',
@@ -25,7 +23,7 @@ app.get('/routes',function(req,res){
     Stream.on('push',function(event, data){
         res.write('event: '+ String(event) +'\n'+'data: '+JSON.stringify(data)+'\n\n');
     });
-});
+})
 let l = 51.673858;
 let a =7.815982;
 
@@ -35,7 +33,7 @@ setInterval(() => {
     let r={
         'lat':l,
         'lng':a,
-        //'timeStamp': new Date()
+        'timeStamp': new Date()
     }
     Stream.emit('push','message',{route:r});
 }, 1000);
